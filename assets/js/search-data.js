79117a7c6e4302682a444b2855bb7946de6bcc34
minhdang26403 <86156224+minhdang26403@users.noreply.github.com>
2025-11-11 16:49:17 +0000

    Deploying to gh-pages from @ minhdang26403/minhdang26403.github.io@c90b464103792f80423dc27b23a133cc273cff01 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/2025-11-11-ndarray-design/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
